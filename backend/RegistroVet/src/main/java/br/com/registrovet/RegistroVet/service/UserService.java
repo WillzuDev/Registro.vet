24674837577d180cf9fb68ba615d70ca193adfb8
Will Souza <WillansF22@gmail.com>
2023-05-14 15:42:59 -0300

    Create entity, CRUD and some classes

---
+++ backend/RegistroVet/src/main/java/br/com/registrovet/RegistroVet/service/UserService.java
@@ -0,0 +1,41 @@
+package br.com.registrovet.RegistroVet.service;
+
+import br.com.registrovet.RegistroVet.dto.UserDTO;
+import br.com.registrovet.RegistroVet.model.User;
+import br.com.registrovet.RegistroVet.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    public UserDTO saveUser(UserDTO userDTO) {
+        User user = convertToUser(userDTO);
+        User savedUser = userRepository.save(user);
+        return convertToUserDTO(savedUser);
+    }
+
+    private User convertToUser(UserDTO userDTO) {
+        User user = new User();
+        user.setId(userDTO.getId());
+        user.setName(userDTO.getName());
+        user.setEmail(userDTO.getEmail());
+        user.setPassword(passwordEncoder.encode(userDTO.getPassword()));
+        return user;
+    }
+
+    private UserDTO convertToUserDTO(User user) {
+        UserDTO userDTO = new UserDTO();
+        userDTO.setId(user.getId());
+        userDTO.setName(user.getName());
+        userDTO.setEmail(user.getEmail());
+        userDTO.setPassword(user.getPassword());
+        return userDTO;
+    }
+}
