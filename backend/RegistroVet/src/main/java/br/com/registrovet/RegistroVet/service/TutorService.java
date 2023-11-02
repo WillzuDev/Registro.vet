2c0542c1ffd85f134101d8c886993896c7ecf6e4
Will Souza <WillansF22@gmail.com>
2023-11-02 19:31:15 -0300

    refact: add format methods and change convert DTO <-> Entity methods to modelMapper

--- backend/RegistroVet/src/main/java/br/com/registrovet/RegistroVet/service/TutorService.java
+++ backend/RegistroVet/src/main/java/br/com/registrovet/RegistroVet/service/TutorService.java
@@ -3,6 +3,7 @@ package br.com.registrovet.RegistroVet.service;
 import br.com.registrovet.RegistroVet.dto.TutorDTO;
 import br.com.registrovet.RegistroVet.model.Tutor;
 import br.com.registrovet.RegistroVet.repository.TutorRepository;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,29 +12,32 @@ public class TutorService {
     @Autowired
     private TutorRepository tutorRepository;
 
+    @Autowired
+    private ModelMapper modelMapper;
+
     public TutorDTO saveTutor (TutorDTO tutorDTO) {
+        tutorDTO.setPhone((formatPhone(tutorDTO.getPhone())));
+        tutorDTO.setCPF((formatCPF(tutorDTO.getCPF())));
+
         Tutor tutor = convetToTutor(tutorDTO);
         Tutor savedTutor = tutorRepository.save(tutor);
+
         return convertToTutorDTO(savedTutor);
     }
 
     private Tutor convetToTutor(TutorDTO tutorDTO) {
-        Tutor tutor = new Tutor();
-        tutor.setId(tutorDTO.getId());
-        tutor.setName(tutorDTO.getName());
-        tutor.setCPF(tutorDTO.getCPF());
-        tutor.setPhone(tutorDTO.getPhone());
-        tutor.setAddress(tutorDTO.getAddress());
-        return tutor;
+        return modelMapper.map(tutorDTO, Tutor.class);
     }
 
     private TutorDTO convertToTutorDTO(Tutor tutor) {
-        TutorDTO tutorDTO = new TutorDTO();
-        tutorDTO.setId(tutor.getId());
-        tutorDTO.setName(tutor.getName());
-        tutorDTO.setCPF(tutor.getCPF());
-        tutorDTO.setPhone(tutor.getPhone());
-        tutorDTO.setAddress(tutor.getAddress());
-        return tutorDTO;
+        return modelMapper.map(tutor, TutorDTO.class);
+    }
+
+    public String formatPhone(String phone) {
+        return phone.replaceAll("[^0-9]", "");
+    }
+
+    public String formatCPF(String CPF) {
+        return CPF.replaceAll("[^0-9]", "");
     }
 }
